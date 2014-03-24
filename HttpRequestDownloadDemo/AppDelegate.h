ffa66a8758240473ec07f01b3d15d234861d695d
MagicLGD <MagicLGD@protekiMacBook-Pro.local>
2014-03-24 15:42:26 +0800

    Initial Commit

---
+++ HttpRequestDownloadDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HttpRequestDownloadDemo
+//
+//  Created by MagicLGD on 14-3-24.
+//  Copyright (c) 2014年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HttpRequestDownloadDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HttpRequestDownloadDemo
+//
+//  Created by MagicLGD on 14-3-24.
+//  Copyright (c) 2014年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
